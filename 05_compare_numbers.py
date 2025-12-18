54347e2b88d5962a11e942daae62563ce0dde951
huriozdil3-droid <huriozdil3@gmail.com>
2025-12-18 13:21:14 +0300

    add number comparison program

---
+++ 05_compare_numbers.py
@@ -0,0 +1,11 @@
+number1 = float(input("number1: "))
+number2 = float(input("number2: "))
+if number1 > number2:
+    print(f" {number1} is bigger than {number2}")
+elif number2 > number1:
+    print(f" {number2} is bigger than {number1}")
+else:
+    print("Numbers are equal")
+
+
+
